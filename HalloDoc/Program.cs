ca8bfc92cd2e542cec4b69e57a62d4308708d93c
Ishan Bhatt <ishan.bhatt@etatvasoft.com>
2024-02-06 18:46:40 +0530

    database integration

--- HalloDoc/Program.cs
+++ HalloDoc/Program.cs
@@ -1,7 +1,13 @@
+using HalloDoc.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<HalloDocDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("HalloDocDbContext")));
 
 var app = builder.Build();
 
